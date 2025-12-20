82920d0e988e91af9c1a1fe67745055ebf8610fc
Traci L Johnson <tracibsnccm@gmail.com>
2025-12-20 01:46:03 -0600

    Refactor index.tsx for cleaner root element handling

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
-// src/index.tsx
-
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Root element not found");
 
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
